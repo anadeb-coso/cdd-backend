94022718259f6bb8484d01e93be6a51701fb8e9d
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2022-10-26 10:26:53 +0200

    model adaptation for test mode

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -16,6 +16,7 @@ class Facilitator(models.Model):
     password = models.CharField(max_length=128, verbose_name=_('password'))
     code = models.CharField(max_length=6, unique=True, verbose_name=_('code'))
     active = models.BooleanField(default=False, verbose_name=_('active'))
+    test_mode = models.BooleanField(default=False, verbose_name=_('test mode'))
 
     __current_password = None
 
