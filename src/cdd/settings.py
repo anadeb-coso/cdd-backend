f98bd69d4b21e543bbfd13e6a8738af3bee4a71d
Jorge Garcia <jorgedgb@gmail.com>
2022-12-03 14:34:51 +0100

    Move /static to /dashboard for DigitalOcean deploy

--- src/cdd/settings.py
+++ src/cdd/settings.py
@@ -140,7 +140,7 @@ DATE_INPUT_FORMATS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
