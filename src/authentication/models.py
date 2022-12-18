5a463cee33bc33667a609b16699c8aa2775783ac
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2022-12-18 15:17:25 +0100

    missing migration

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -14,7 +14,7 @@ class Facilitator(models.Model):
     no_sql_db_name = models.CharField(max_length=150, unique=True)
     username = models.CharField(max_length=150, unique=True, verbose_name=_('username'))
     password = models.CharField(max_length=128, verbose_name=_('password'))
-    code = models.CharField(max_length=6, unique=True, verbose_name=_('code'))
+    code = models.CharField(max_length=6, unique=False, verbose_name=_('code'))
     active = models.BooleanField(default=False, verbose_name=_('active'))
     develop_mode = models.BooleanField(default=False, verbose_name=_('test mode'))
     training_mode = models.BooleanField(default=False, verbose_name=_('test mode'))
