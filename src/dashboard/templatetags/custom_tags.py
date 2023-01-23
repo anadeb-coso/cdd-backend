1b9c537ce08c818da48013650ba1499a707cd4a9
adaboubvincent <adaboubvincent@gmail.com>
2023-01-23 23:06:08 +0000

    Setuped the filter function to get fields with values for a task

--- src/dashboard/templatetags/custom_tags.py
+++ src/dashboard/templatetags/custom_tags.py
@@ -74,3 +74,47 @@ def get_hour(date_time):
     if data:
         data = data[1].split('.')[0]
     return data
+
+
+@register.filter(name="structureTheFields")
+def structure_the_fields(task):
+    fields_values = {}
+    if task.get("form_response"):
+        for fields in task.get("form_response"):
+            for field, value in fields.items():
+                if type(value) in (dict, list):
+                    if type(value) == list:
+                        for l_field in value:
+                            for field1, value1 in l_field.items():
+                                if type(value1) in (dict, list):
+                                    if type(value1) == list:
+                                        for l_field in value1:
+                                            for field2, value2 in l_field.items():
+                                                fields_values[field2] = value2
+                                    else:
+                                        for field3, value3 in value1.items():
+                                            if type(value3) == list:
+                                                for l_field in value3:
+                                                    for field4, value4 in l_field.items():
+                                                        fields_values[field4] = value4
+                                            else:
+                                                fields_values[field3] = value3
+                                else:
+                                    fields_values[field1] = value1
+
+                    else:
+                        for field5, value5 in value.items():
+                            if type(value5) in (dict, list):
+                                if type(value5) == list:
+                                    for l_field in value5:
+                                        for field6, value6 in l_field.items():
+                                            fields_values[field6] = value6
+                                else:
+                                    for field7, value7 in value5.items():
+                                        fields_values[field7] = value7
+                            else:
+                                fields_values[field5] = value5
+                else:
+                    fields_values[field] = value
+                    
+    return fields_values
