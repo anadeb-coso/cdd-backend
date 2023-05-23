b6e83496db866936dc388d145f6caa3528288de6
adaboubvincent <adaboubvincent@gmail.com>
2023-05-23 19:42:08 +0000

    Added migrations files and added models on admin interface

--- src/process_manager/admin.py
+++ src/process_manager/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Phase, Project, Activity, Task
+from .models import *
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Phase)
 admin.site.register(Activity)
 admin.site.register(Task)
+
+
+admin.site.register(Wave)
+admin.site.register(Deployment)
+admin.site.register(AdministrativeLevelWave)
+admin.site.register(FacilitatorWave)
+admin.site.register(FacilitatorDeployment)
