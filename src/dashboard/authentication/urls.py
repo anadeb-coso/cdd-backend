6937404a71ad938385dce3f10e8b335cb06aebdc
adaboubvincent <adaboubvincent@gmail.com>
2023-05-30 10:57:43 +0000

    Setuped the urls, html files to manage creation and list users

--- src/dashboard/authentication/urls.py
+++ src/dashboard/authentication/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from dashboard.authentication.forms import EmailAuthenticationForm
+from dashboard.authentication import views
 
 app_name = 'authentication'
 urlpatterns = [
@@ -9,5 +10,7 @@ urlpatterns = [
         authentication_form=EmailAuthenticationForm,
         template_name='authentication/login.html',
         redirect_authenticated_user=True), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('users/', views.UsersListView.as_view(), name='users'),
+    path('user-create/', views.CreateUserFormView.as_view(), name='user_create')
 ]
