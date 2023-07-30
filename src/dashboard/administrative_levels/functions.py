fedfb03e9d2694c880cac1ab3198ea245b395c34
adaboubvincent <adaboubvincent@gmail.com>
2023-07-30 14:19:14 +0000

    Updated get_cascade_villages_by_administrative_level_id funtion to take param list

--- src/dashboard/administrative_levels/functions.py
+++ src/dashboard/administrative_levels/functions.py
@@ -19,37 +19,44 @@ def get_administrative_levels_under_json(administrative_levels):
 
 
 
-def get_cascade_villages_by_administrative_level_id(_id):
-    if _id:
-        ad_obj = administrativelevels_models.AdministrativeLevel.objects.using('mis').get(id=int(_id))
-        
-        if ad_obj:
-            ads = []
-            _type = ad_obj.type
-            if _type == "Village":
-                ads.append(ad_obj)
-            else:
-                ads = ad_obj.administrativelevel_set.get_queryset()
+def get_cascade_villages_by_administrative_level_id(_ids):
+    print(_ids, "first")
+    if type(_ids) is not list:
+        _ids = [_ids]
+    if _ids:
+        print(_ids)
+        ad_objects = administrativelevels_models.AdministrativeLevel.objects.using('mis').filter(id__in=[int(_id) for _id in _ids if _id])
+        print(ad_objects)
+        villages = []
+        for ad_obj in ad_objects:
+            if ad_obj:
+                ads = []
+                _type = ad_obj.type
+                if _type == "Village":
+                    ads.append(ad_obj)
+                else:
+                    ads = ad_obj.administrativelevel_set.get_queryset()
+                    
+                datas = {
+                    "prefectures": ads if _type == "Region" else [], 
+                    "communes": ads if _type == "Prefecture" else [], 
+                    "cantons": ads if _type == "Commune" else [], 
+                    "villages": ads if _type in ("Canton", "Village") else []
+                }
+                for p in datas["prefectures"]:
+                    [datas["communes"].append(o) for o in p.administrativelevel_set.get_queryset()]
+
+                for c in datas["communes"]:
+                    [datas["cantons"].append(o) for o in c.administrativelevel_set.get_queryset()]
                 
-            datas = {
-                "prefectures": ads if _type == "Region" else [], 
-                "communes": ads if _type == "Prefecture" else [], 
-                "cantons": ads if _type == "Commune" else [], 
-                "villages": ads if _type in ("Canton", "Village") else []
-            }
-            for p in datas["prefectures"]:
-                [datas["communes"].append(o) for o in p.administrativelevel_set.get_queryset()]
+                for c in datas["cantons"]:
+                    [datas["villages"].append(o) for o in c.administrativelevel_set.get_queryset()]
+                
+                if _type == "village":
+                    datas["villages"].append(ad_obj)
+                villages += datas["villages"]
 
-            for c in datas["communes"]:
-                [datas["cantons"].append(o) for o in c.administrativelevel_set.get_queryset()]
-            
-            for c in datas["cantons"]:
-                [datas["villages"].append(o) for o in c.administrativelevel_set.get_queryset()]
-            
-            if _type == "village":
-                datas["villages"].append(ad_obj)
-
-            return get_administrative_levels_under_json(datas["villages"])
+        return get_administrative_levels_under_json(list(set(villages)))
     return []
 
 def get_cascade_administrative_levels_by_administrative_level_id(_id):
