07ac60374fa44f2237b9b43a9c8660e5f14151f1
adaboubvincent <adaboubvincent@gmail.com>
2024-03-25 10:12:48 +0000

    Changed '; ' to ';'

--- src/dashboard/statistics/functions.py
+++ src/dashboard/statistics/functions.py
@@ -286,7 +286,7 @@ def get_global_statistic_under_file_excel_or_csv(facilitator_db_name, file_type=
                             # villages = ""
                             # for o in administrativelevel_obj.cvd.get_villages():
                             #     villages += f'{o.name} ; '
-                            datas[("FICHE DE SUIVI MENSUEL DES INDICATGEURS DES RÉUNIONS CANTONNALES/VILLAGEOISES", "LOCALITE", "Villages", "Villages", "Villages", "Villages", "ind_6")][count] = "; ".join([o.name for o in administrativelevel_obj.cvd.get_villages()])
+                            datas[("FICHE DE SUIVI MENSUEL DES INDICATGEURS DES RÉUNIONS CANTONNALES/VILLAGEOISES", "LOCALITE", "Villages", "Villages", "Villages", "Villages", "ind_6")][count] = ";".join([o.name for o in administrativelevel_obj.cvd.get_villages()])
                             datas[("FICHE DE SUIVI MENSUEL DES INDICATGEURS DES RÉUNIONS CANTONNALES/VILLAGEOISES", "LOCALITE", "Unité géographique", "Unité géographique", "Unité géographique", "Unité géographique", "ind_7")][count] = administrativelevel_obj.geographical_unit.attributed_number_in_canton
                             datas[("FICHE DE SUIVI MENSUEL DES INDICATGEURS DES RÉUNIONS CANTONNALES/VILLAGEOISES", "LOCALITE", "Nom de l'AC", "Nom de l'AC", "Nom de l'AC", "Nom de l'AC", "ind_8")][count] = f.name
                             
