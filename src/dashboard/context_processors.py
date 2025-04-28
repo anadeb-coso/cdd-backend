f78d8f34b96548b8875307eeb20c5782b32e1140
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2025-04-28 10:29:33 +0000

    Update global variables functions

--- src/dashboard/context_processors.py
+++ src/dashboard/context_processors.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from process_manager.models import Project
+from process_manager.models import Project, Cycle
 from authentication import FACILITATORS_TYPES_PLURAL
 
 
@@ -12,7 +12,16 @@ def settings_vars(request):
         "PROJECT_NAME": request.session.get('project_name'),
         "PROJECT_COUCH_ID": request.session.get('project_couch_id'),
         
-        "PROJECTS": Project.objects.all(),
+        "PROJECT_MIS_ID": request.session.get('project_mis_id'),
+
+        "CYCLE_ID": request.session.get('cycle_id'),
+        "CYCLE_NAME": request.session.get('cycle_name'),
+        "CYCLE_COUCH_ID": request.session.get('cycle_couch_id'),
+        
+        "CYCLE_MIS_ID": request.session.get('cycle_mis_id'),
+        
+        "PROJECTS": Project.objects.filter(users__in=[request.user.id]) if request.user.is_authenticated else [],
+        "CYCLES": Cycle.objects.filter(project_id=request.session.get('project_id')),
 
         "CDD_URL_BASE": settings.CDD_URL_BASE,
         "MIS_URL_BASE": settings.MIS_URL_BASE,
