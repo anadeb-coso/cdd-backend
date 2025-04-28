546baeccf0f301b626718cd609db85b8ebec2b33
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2025-04-28 10:30:52 +0000

    Add cycle class and add news attributes on Project class

--- src/subprojects/models.py
+++ src/subprojects/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+from administrativelevels.models import AdministrativeLevel
 
 from cdd.models_base import BaseModel
 
@@ -17,8 +17,21 @@ class Financier(BaseModel):
 class Project(BaseModel):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    financier = models.ForeignKey('Financier', null=True, on_delete=models.CASCADE)
+    financiers = models.ManyToManyField('Financier', default=[], blank=True, related_name="financiers_projects")
+
+    administrative_levels = models.ManyToManyField(AdministrativeLevel, default=[], blank=True, verbose_name=_("Administrative Levels"), related_name="administrative_levels_projects")
 
     def __str__(self):
         return self.name
 
+
+class Cycle(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    project_id = models.IntegerField()
+    
+    administrative_levels = models.ManyToManyField(AdministrativeLevel, default=[], blank=True, verbose_name=_("Administrative Levels"), related_name="administrative_levels_cycles")
+
+
+    def __str__(self):
+        return self.name
