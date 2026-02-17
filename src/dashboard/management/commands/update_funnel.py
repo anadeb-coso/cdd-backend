19de925fbde8538d71e33c98bf7c46a78cb4a570
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2026-02-17 11:30:04 +0000

    Updated update_funnel command to include input questions and call the right functions

--- src/dashboard/management/commands/update_funnel.py
+++ src/dashboard/management/commands/update_funnel.py
@@ -1,10 +1,83 @@
 from django.core.management.base import BaseCommand
 import os
-from dashboard.tasks import sync_celery_tasks
-
+from dashboard.tasks import sync_celery_tasks_re, sync_aggregated_status_on_adl
+from process_manager.models import Project, Cycle, AggregatedStatusFacilitator
 
 class Command(BaseCommand):
     help = 'Update the AggregatedStatus objects for the funnel'
 
+    def manage_prompt(self, type_value, prompt_message, options=None, default=''):
+        if options:
+            prompt_message += f" ({options})"
+        prompt_message += " : "
+        while True:
+            try:
+                value = input(prompt_message) or default
+                if type_value == bool:
+                    return bool(int(value))
+                elif type_value == int:
+                    return int(value)
+                elif type_value == list:
+                    return [elt.strip().lower() for elt in value.split(",") if elt.strip().lower()]
+                else:
+                    return value
+            except ValueError:
+                self.stdout.write(self.style.ERROR(f"Invalid input. Please enter a valid {type_value.__name__} value."))
+
     def handle(self, *args, **options):
-        sync_celery_tasks()
+        self.stdout.write(self.style.WARNING('\n\n\t\t**************************** Welcome to synchronize command processes! ****************************'))
+        self.stdout.write(self.style.WARNING('You must provide the project_id, cycle_id, develop_mode, training_mode, no_sql_dbs to synchronize the tasks on facilitators db and update the aggregated status on adl!'))
+        self.stdout.write(self.style.WARNING('After providing db names, you will be asked if you want to continue the process or not, you can add as many projects as you want before starting the synchronization process!'))
+
+        projects_queue_list = []
+        count = 1
+        while True:
+            self.stdout.write(self.style.WARNING(f'\n\n\t\t**************************** Project {count} ****************************'))
+            project_id = self.manage_prompt(int, f"\nproject_id [Ex.: 3] ({[{p.id: p.name} for p in Project.objects.all()]})")
+            cycle_id = self.manage_prompt(int, f"cycle_id [Ex.: 3] ({[{c.id: c.__str__()} for c in Cycle.objects.filter(project_id=project_id)]})")
+            develop_mode = self.manage_prompt(bool, "develop_mode [0=False, 1=True] (default is False)", default='0')
+            training_mode = self.manage_prompt(bool, "training_mode [0=False, 1=True] (default is False)", default='0')
+            no_sql_dbs = self.manage_prompt(list, "facilitators db [Ex. : facilitator_1233456,facilitator_12889556, facilitator_1277855]")
+
+            projects_queue_list.append((project_id, cycle_id, develop_mode, training_mode, no_sql_dbs))
+
+            continue_process = self.manage_prompt(bool, "\ncontinue adding process [0=False, 1=True] (default is False)", default='0')
+            if not continue_process:
+                break
+            count += 1
+
+        self.stdout.write(self.style.WARNING('\n\n\t\tYou want to synchronize the following projects with their respective cycles and no_sql_dbs?'))
+        for project_id, cycle_id, develop_mode, training_mode, no_sql_dbs in projects_queue_list:
+            self.stdout.write(self.style.WARNING(f"\t- Project ID: {project_id}, Cycle ID: {cycle_id}, Develop Mode: {develop_mode}, Training Mode: {training_mode}, No SQL DBs: {no_sql_dbs}"))
+        
+        confirm = self.manage_prompt(bool, "\nconfirm [0=False, 1=True] (default is True)", default='1')
+        if not confirm:
+            self.stdout.write(self.style.ERROR('\nProcess cancelled by the user.'))
+            return
+        
+        execute_sync_celery_tasks_re_function = self.manage_prompt(bool, "\nDid you want to execute sync_celery_tasks_re function [0=False, 1=True] (default is True)", default='1')
+        execute_sync_aggregated_status_on_adl_function = self.manage_prompt(bool, "\nDid you want to execute sync_aggregated_status_on_adl function [0=False, 1=True] (default is True)", default='1')
+        if execute_sync_aggregated_status_on_adl_function:
+            execute_adl_village = self.manage_prompt(bool, "\nDid you want to execute adl village [0=False, 1=True] (default is True)", default='1')
+            execute_adl_bigger_than_village = self.manage_prompt(bool, "\nDid you want to execute adl bigger than village (canton, commune, prefecture, region) [0=False, 1=True] (default is False)", default='0')
+
+        project_id, cycle_id, develop_mode, training_mode, no_sql_dbs = None, None, None, None, None
+        for project_id, cycle_id, develop_mode, training_mode, no_sql_dbs in projects_queue_list:
+            self.stdout.write(self.style.WARNING("\n\n====================================================\n"))
+            self.stdout.write(self.style.WARNING(f'\tStarting synchronization for Project ID: {project_id}, Cycle ID: {cycle_id}...'))
+            
+            if execute_sync_celery_tasks_re_function:
+                if no_sql_dbs:
+                    for no_sql_db in no_sql_dbs:
+                        sync_celery_tasks_re(project_id, cycle_id, develop_mode, training_mode, no_sql_db)
+                else:
+                    sync_celery_tasks_re(project_id, cycle_id, develop_mode, training_mode)
+            if execute_sync_aggregated_status_on_adl_function:
+                sync_aggregated_status_on_adl(project_id, cycle_id, execute_adl_village, execute_adl_bigger_than_village)
+            
+            if execute_sync_celery_tasks_re_function or execute_sync_aggregated_status_on_adl_function:
+                AggregatedStatusFacilitator.objects.filter(project_id=project_id, cycle_id=cycle_id).update(new_update_exists=True)
+            
+
+        self.stdout.write(self.style.SUCCESS('Successfully executed update_funnel command!'))
+
