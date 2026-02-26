ede267289fd8d7acba94e0bbccbd6bc4de2c067b
Jorge García <jorgedgb@gmail.com>
2026-02-26 15:46:57 +0100

    test: add missing pytest configuration files

---
+++ src/cdd/test_settings.py
@@ -0,0 +1,30 @@
+# ruff: noqa: F405
+import logging
+
+try:
+    from cdd.settings import *  # noqa
+except ImportError:
+    pass
+
+MIDDLEWARE = [
+    middleware
+    for middleware in MIDDLEWARE
+    if middleware
+    not in [  # noqa: F405
+        "django.middleware.security.SecurityMiddleware",
+        "corsheaders.middleware.CorsMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    ]
+]
+
+INSTALLED_APPS = [app for app in INSTALLED_APPS if app not in ["django.contrib.staticfiles"]]  # noqa: F405
+
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
+
+MEDIA_URL = "/media/"
+
+logging.disable(logging.CRITICAL)
+
+LANGUAGE_CODE = "en-us"
